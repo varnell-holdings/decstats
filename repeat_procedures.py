5ddeea2d75bbe428b45f52d36281b45b16f58b5a
varnell-holdings <john.lamia@gmail.com>
2025-01-15 00:48:54 +1100

    improved++

--- repeat_procedures.py
+++ repeat_procedures.py
@@ -11,34 +11,51 @@ def stringify(m):
     return m
 
 
-def month_finder(month):
+def dates_finder(month):
+    z = month - 3
     a = month - 2
     b = month - 1
     c = month
+    flip_flag = False
+    if c == 3:
+        z = 12
+        flip_flag = True
 
-    return {stringify(a), stringify(b), stringify(c)}
+    return {stringify(a), stringify(b), stringify(c)}, stringify(z), flip_flag
 
 
 def find_repeats(year, month):
-    month_set = month_finder(month)
-    procedures = defaultdict(int)
+    month_set, z, flip_flag = dates_finder(month)
+    procedures = defaultdict(list)
     admissions = 0
     repeat_patients = 0
     with open("day_surgery.csv") as fh:
         reader = csv.reader(fh)
         for episode in reader:
             if (episode[0][6:10] == year and episode[0][3:5] in month_set) or (
-                episode[0][6:10] == str(int(year) - 1) and episode[0][3:5] == "12"
+                flip_flag
+                and episode[0][6:10] == str(int(year) - 1)
+                and episode[0][3:5] == "12"
             ):
                 mrn = episode[1]
-                procedures[mrn] += 1
+                data = (episode[1], episode[0], episode[7], episode[8])
+                if mrn not in procedures:
+                    procedures[mrn].append(data)
+                else:
+                    flat_info = [item for tup in procedures[mrn] for item in tup]
+                    if data[1] not in flat_info:
+                        procedures[mrn].append(data)
                 admissions += 1
-                # print(episode[0])
+
         # print(procedures)
         with open("repeats.txt", "a") as f:
             for key, value in procedures.items():
-                if value > 1:
-                    print(key)
+                if len(value) > 1:
+                    for admission in value:
+                        print(
+                            f"{admission[0].ljust(15)} {admission[1].ljust(15)} {admission[2].ljust(15)} {admission[3].ljust(15)}"
+                        )
+                    print()
                     repeat_patients += 1
                     f.write(key + "\n")
     print(f"Number of admissions: {admissions}")
@@ -54,8 +71,10 @@ def intro():
             break
 
     while True:
-        month = int(input("Enter the month to finish survey as a number: "))
-        if month < 13:
+        month = int(
+            input("Enter the month to finish survey as a number, 3, 6, 9, 12: ")
+        )
+        if month in {3, 6, 9, 12}:
             break
 
     return year, month
